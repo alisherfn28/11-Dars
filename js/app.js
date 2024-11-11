1a8f632090c40486527a700679c7a68dc43321ba
alisher <alisher@gmail.com>
2024-11-11 17:24:14 +0500

    11 dars

--- js/app.js
+++ js/app.js
@@ -12,42 +12,42 @@ const button = document.querySelector("#btn");
 
 //!comments
 
-// function createBlock(value) {
-//   return `
-//     <div class="block">
-//             <h3>${value.name}</h3>
-//             <h3${value.email}</h3>
-//             <p>${value.body}</p>
-//             <span>${value.id}</span>
-//         </div>
-//     `;
-// }
+function createBlock(value) {
+  return `
+    <div class="block">
+            <h3>${value.name}</h3>
+            <h3${value.email}</h3>
+            <p>${value.body}</p>
+            <span>${value.id}</span>
+        </div>
+    `;
+}
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   fetch("https://jsonplaceholder.typicode.com/comments", {
-//     method: "GET",
-//   })
-//     .then((response) => {
-//       if (response.status == 200) {
-//         return response.json();
-//       }
-//       if (response.status == 404) {
-//         throw new Error("API ga noto'g'ri murojat qilindi");
-//       }
-//     })
-//     .then((data) => {
-//       data.forEach((element) => {
-//         let card = createBlock(element);
-//         wrapper.innerHTML += card;
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     })
-//     .finally(() => {
-//       console.log("API ga murjat tugadi");
-//     });
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("https://jsonplaceholder.typicode.com/comments", {
+    method: "GET",
+  })
+    .then((response) => {
+      if (response.status == 200) {
+        return response.json();
+      }
+      if (response.status == 404) {
+        throw new Error("API ga noto'g'ri murojat qilindi");
+      }
+    })
+    .then((data) => {
+      data.forEach((element) => {
+        let card = createBlock(element);
+        wrapper.innerHTML += card;
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      console.log("API ga murjat tugadi");
+    });
+});
 
 //!posts
 
